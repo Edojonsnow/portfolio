bc44a9661de351bd80f91a1b22c8cb2fd2e15318
Alexander Oronsaye <osahenrumeh13@gmail.com>
2025-01-25 00:53:50 +0100

    added whatsapp shortlink

--- src/components/Home.js
+++ src/components/Home.js
@@ -117,7 +117,7 @@ const Home = () => {
     },
     {
       label: "What'sApp",
-      href: "#",
+      href: "https://wa.link/lgb6ji",
       icon: (
         <IconBrandWhatsapp className="text-neutral-500 purple-icon hover:text-purple-500   dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
